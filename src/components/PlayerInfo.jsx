aa719da6989af23be18cbdc19e512ba8c06c03e0
Anvar Artykov <anvar@aartykoDQD3H29K.attlocal.net>
2023-12-09 21:58:55 -0800

    Bug fix

--- src/components/PlayerInfo.jsx
+++ src/components/PlayerInfo.jsx
@@ -4,7 +4,7 @@ import { Typography } from "@mui/material";
 const PlayerInfo = ({ player, matches, turns }) => {
   return (
     <div>
-      <Typography variant='h4' style={{ color: turns ? "green" : "" }}>
+      <Typography variant='h4' sx={{ color: turns ? "green" : "" }}>
         {player}: {`Matches: ${matches}`}
       </Typography>
     </div>
